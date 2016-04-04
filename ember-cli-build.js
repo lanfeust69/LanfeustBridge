0f5f7e12b3a2544517230c5a9f525e58cd8dd3a4
Jean-Jacques Lafay <jeanjacques.lafay@gmail.com>
2016-04-04 19:57:34 +0200

    Add ng2-bootstrap

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,10 @@ var Angular2App = require('angular-cli/lib/broccoli/angular2-app');
 
 module.exports = function(defaults) {
   var app = new Angular2App(defaults, {
-    vendorNpmFiles: []
+    vendorNpmFiles: [
+      'ng2-bootstrap/bundles/ng2-bootstrap.min.js',
+      'moment/moment.js'
+    ]
   });
   return app.toTree();
 };
--- src/app/front-end.ts
+++ src/app/front-end.ts
@@ -1,12 +1,13 @@
 import {Component} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
 import {CliRouteConfig} from './route-config';
+import {Alert, DATEPICKER_DIRECTIVES} from 'ng2-bootstrap/ng2-bootstrap';
 
 @Component({
   selector: 'front-end-app',
   providers: [ROUTER_PROVIDERS],
   templateUrl: 'app/front-end.html',
-  directives: [ROUTER_DIRECTIVES],
+  directives: [ROUTER_DIRECTIVES, Alert],
   pipes: []
 })
 @RouteConfig([
