604d0a1c5c337e37c6223b4daaa5f4b950245656
Jean-Jacques Lafay <jeanjacques.lafay@gmail.com>
2018-04-14 13:15:52 +0200

    Customize Identity UI
    Microsoft.AspNetCore.Identity really conflates UserName and Email, and editing
    the Email results in Bad Things, so show only one (read-only), but add an
    (editable) DisplayName field.

--- Models/User.cs
+++ Models/User.cs
@@ -6,6 +6,7 @@ namespace LanfeustBridge.Models
 {
     public class User : IdentityUser
     {
+        public string DisplayName { get; set; }
         // use strings to help indexing
         public List<string> ExternalLogins { get; set; } = new List<string>();
     }
