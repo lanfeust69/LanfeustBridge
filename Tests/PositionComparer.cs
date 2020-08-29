68bef48935d81746d8d4fa8ba750ef93529415a3
Jean-Jacques Lafay <jeanjacques.lafay@gmail.com>
2020-08-29 11:43:46 +0200

    Update front-end packages
    Notably update to Angular 10.

--- Tests/PositionComparer.cs
+++ Tests/PositionComparer.cs
@@ -7,8 +7,12 @@ namespace LanfeustBridge.Tests
 
     internal class PositionComparer : IEqualityComparer<Position>
     {
-        public bool Equals(Position x, Position y)
+        public bool Equals(Position? x, Position? y)
         {
+            if (x == null && y == null)
+                return true;
+            if (x == null || y == null)
+                return false;
             if (x.Table != y.Table)
                 return false;
             if (x.Deals.Length != y.Deals.Length || x.Deals.Zip(y.Deals, (a, b) => a != b).Any(b => b))
